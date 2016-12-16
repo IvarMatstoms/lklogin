75f404a6ae9d5eedc624d98889b7162a632d2ade
Ivar Matstoms <ivar.matstoms@gmail.com>
2016-12-16 13:13:46 +0000

    added python 3 support

--- lklogin.py
+++ lklogin.py
@@ -1,10 +1,21 @@
-import urllib2
+import sys
 import urllib
 username="username"
 password="password"
+
 url="https://surf.linkoping.se:6082/php/uid.php?vsys=1&url=http://google.se"
 values={"inputStr":'',"user":username,"escapeUser":username,"passwd":password,"ok":"Login"}
-data=urllib.urlencode(values)
-req=urllib2.Request(url,data)
-resp=urllib2.urlopen(req)
-resptxt=resp.read()
+
+if (sys.version_info > (3, 0)):
+    import urllib.parse
+    import urllib.request
+    data = urllib.parse.urlencode(values).encode()
+    req=urllib.request.Request(url,data)
+    resp=urllib.request.urlopen(req)
+    resptxt=resp.read()
+else:
+    import urllib2
+    data=urllib.urlencode(values)
+    req=urllib2.Request(url,data)
+    resp=urllib2.urlopen(req)
+    resptxt=resp.read()
